706e4f4f704734b4e4e4b8b7d784beddc94f5eea
fernando.cervantes <uniXj0nescev>
2018-03-15 09:47:44 -0600

    comments

--- read_json.py
+++ read_json.py
@@ -1,12 +1,12 @@
 import json
 import pandas as pndas
-
+#Reading the json file with json library
 with open('assets/products.json') as json_file:
     data = json.load(json_file)
     print('Reading Json...')
     for p in data:
         print('Product: ' + p['product_name'])
 
-
+#Reading json file with pandas
 file = pndas.read_json('assets/products.json')
 file.to_excel('output.xls', index=False)
