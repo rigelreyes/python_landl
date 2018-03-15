6c61d966038862e3fed750715bd6266360eeab4f
fernando.cervantes <uniXj0nescev>
2018-03-15 09:31:40 -0600

    read json example

--- read_json.py
+++ read_json.py
@@ -1,1 +1,12 @@
-# Fernando
+import json
+import pandas as pndas
+
+with open('assets/products.json') as json_file:
+    data = json.load(json_file)
+    print('Reading Json...')
+    for p in data:
+        print('Product: ' + p['product_name'])
+
+
+file = pndas.read_json('assets/products.json')
+file.to_excel('output.xls', index=False)
