3320f029d02f1c718a1f7b7b62dcd42029d14501
fernando.cervantes <fernando.cervantes@oliverwyman.com>
2018-04-02 14:37:02 -0500

    sympy

---
+++ sympy.py
@@ -0,0 +1,7 @@
+from __future__ import division
+from sympy import *
+x, y, z, t = symbols('x y z t')
+k, m, n = symbols('k m n', integer=True)
+f, g, h = symbols('f g h', cls=Function)
+
+integrate(sin(x**2), (x, -oo, oo))
